436a8b7547b47bf97af86fd01d1ba17940d069ee
hurstie16s <samuel.hurst2@btinternet.com>
2022-09-06 11:46:50 +0100

    Created new javafx maven project

---
+++ organized/src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.hurst {
+    requires javafx.controls;
+    requires java.sql;
+    requires org.apache.logging.log4j;
+    requires mysql.connector.java;
+
+    opens com.hurst to javafx.base;
+
+    exports com.hurst;
+}
