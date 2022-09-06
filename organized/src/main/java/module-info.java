83c4119278562199a08664002c5c452795d82155
hurstie16s <samuel.hurst2@btinternet.com>
2022-09-06 08:06:14 +0100

    Created Java Project Organized

---
+++ organized/src/main/java/module-info.java
@@ -0,0 +1,15 @@
+module com.hurst.organized {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.hurst.organized to javafx.fxml;
+    exports com.hurst.organized;
+}
